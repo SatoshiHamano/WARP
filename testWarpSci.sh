e74b3bbcd8d3d499335246b6f4d3a97e2fc663b4
SatoshiHamano <satoon19@gmail.com>
2023-03-22 15:17:26 +0900

    The python commands in test shell scripts are replaced with the variance.@ " "

--- testWarpSci.sh
+++ testWarpSci.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python Warp_sci.py ./TEST/WIDE/4_Ari_list.txt -r ./TEST/WIDE/ -c ./TEST/WIDE/WINERED_calibration_LCO22b_WIDE100_20220914_v2/ -v ./TEST/WIDE/ -d ./TEST/4_Ari_WIDE_test -f
+PYTHON='python'
+$PYTHON Warp_sci.py ./TEST/WIDE/4_Ari_list.txt -r ./TEST/WIDE/ -c ./TEST/WIDE/WINERED_calibration_LCO22b_WIDE100_20220914_v2/ -v ./TEST/WIDE/ -d ./TEST/4_Ari_WIDE_test -f
