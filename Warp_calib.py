b1011db74c99ba987d5310d5e312d8648ed325f8
SatoshiHamano <satoon19@gmail.com>
2023-08-04 03:39:40 +0900

    sys.exit()

--- Warp_calib.py
+++ Warp_calib.py
@@ -345,7 +345,7 @@ def main(inputfile, aperturereplace, transformonly):
 
         print("[t0, t1, t2], [ap0, ap1, ap2] = [%.3e, %.3e, %.3e], [%.3e, %.3e, %.3e]" % (t0, t1, t2, ap0, ap1, ap2))
         constant_str_length("Calculate transform function")
-
+        sys.exit()
         AP_to_FC(pinholefile, [t0, t1, t2], [ap0, ap1, ap2])
     else:
         constant_str_length("SKIP Calculate transform function")
